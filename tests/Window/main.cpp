604308beb33da4ec435379913d1d49516842bf0c
koya <gnndr36@gmail.com>
2022-11-13 22:08:37 +0900

    Add: ウィンドウ表示テストを追加

---
+++ tests/Window/main.cpp
@@ -0,0 +1,14 @@
+#include <ag/Engine.hpp>
+#include <ag/Window.hpp>
+#include <ag/native/glfw.hpp>
+
+int main(int argc, char* argv[])
+{
+    auto engine = ag::Engine::getInstance()->startup(argc, argv);
+    auto window = ag::Window::create(1280, 720, "");
+    window->makeContextCurrent();
+    while (!window->shouldClose()) {
+        glfwPollEvents();
+    }
+    return 0;
+}
