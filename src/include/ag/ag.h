385afe79426b3d21befdbb3b29045db464f8746f
koya <gnndr36@gmail.com>
2022-11-13 14:57:16 +0900

    Add: テスト用の関数を追加

---
+++ src/include/ag/ag.h
@@ -0,0 +1,7 @@
+#pragma once
+namespace ag {
+/**
+ * @brief print string of "Hello, ag!".
+ */
+void hello();
+} // namespace ag
---
+++ src/lib/ag/ag.cpp
@@ -0,0 +1,6 @@
+#include <ag.h>
+#include <iostream>
+
+namespace ag {
+void hello() { std::cout << "Hello, ag!" << std::endl; }
+} // namespace ag
