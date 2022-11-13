dd648e9fdb381df336da53fe97c15a2ba328cc5f
koya <gnndr36@gmail.com>
2022-11-13 22:08:20 +0900

    Add: エンジンクラスを追加

---
+++ src/include/ag/Engine.hpp
@@ -0,0 +1,51 @@
+#pragma once
+#include <memory>
+#include <thread>
+
+namespace ag {
+class IGraphicsDriver;
+/**
+ * @brief Engine class is ag Engine.
+ */
+class Engine {
+public:
+    using Instance = std::shared_ptr<Engine>;
+
+    ~Engine();
+    /**
+     * @brief Get the Engine object
+     * @return Instance
+     */
+    static Instance getInstance();
+
+    /**
+     * @brief throw exception if instance not exist.
+     */
+    static void require();
+
+    /**
+     * @brief startup a ag Engine.
+     * @param argc
+     * @param argv
+     */
+    Instance startup(int argc, char* argv[]);
+
+    /**
+     * @brief shutdown a ag Engine.
+     */
+    void shutdown();
+
+    /**
+     * @brief Get the Graphics Driver object
+     * @return std::shared_ptr<IGraphicsDriver>
+     */
+    std::shared_ptr<IGraphicsDriver> getGraphicsDriver() const;
+
+private:
+    static std::mutex s_mutex;
+    static Instance s_instance;
+    bool m_started;
+    std::shared_ptr<IGraphicsDriver> m_graphicsDriver;
+    Engine();
+};
+}
---
+++ src/lib/ag/Engine.cpp
@@ -0,0 +1,72 @@
+#include <ag/Engine.hpp>
+#include <ag/Window.hpp>
+#include <ag/graphics/ogl/OglGraphicsDriver.hpp>
+#include <ag/native/glfw.hpp>
+#include <stdexcept>
+
+namespace ag {
+std::mutex Engine::s_mutex;
+Engine::Instance Engine::s_instance = nullptr;
+
+Engine::~Engine()
+{
+    shutdown();
+}
+
+Engine::Instance Engine::getInstance()
+{
+    std::lock_guard<std::mutex> lock(s_mutex);
+    if (!s_instance) {
+        s_instance = Engine::Instance(new Engine());
+    }
+    return s_instance;
+}
+
+void Engine::require()
+{
+    if (!s_instance || !s_instance->m_started) {
+        throw std::logic_error("Engine is not exist.");
+    }
+}
+
+Engine::Instance Engine::startup(int argc, char* argv[])
+{
+    // startup runs a only at first call.
+    if (m_started) {
+        return nullptr;
+    }
+    m_started = true;
+    // initialize glfw system.
+    int glfwStatus = glfwInit();
+    if (glfwStatus == GLFW_FALSE) {
+        return nullptr;
+    }
+    return s_instance;
+}
+
+void Engine::shutdown()
+{
+    if (!m_started) {
+        return;
+    }
+    // destroy all window.
+    std::vector<Window::Instance> windows = Window::getWindows();
+    std::for_each(windows.begin(), windows.end(), std::bind(&Window::dispose, std::placeholders::_1));
+    // terminalte glfw.
+    glfwTerminate();
+    s_instance = nullptr;
+}
+
+std::shared_ptr<IGraphicsDriver> Engine::getGraphicsDriver() const
+{
+    return m_graphicsDriver;
+}
+
+// private
+Engine::Engine()
+    : m_started(false)
+    , m_graphicsDriver()
+{
+    m_graphicsDriver = std::make_shared<OglGraphicsDriver>();
+}
+}
