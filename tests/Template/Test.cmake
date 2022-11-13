077b826905891b2b258680c5a098ae0e1e4cbc0c
koya <gnndr36@gmail.com>
2022-11-13 16:05:42 +0900

    Add: テスト用のテンプレートを追加

---
+++ tests/Template/Test.cmake
@@ -0,0 +1,36 @@
+cmake_minimum_required(VERSION 3.10)
+project(${T_APP_NAME} CXX)
+
+set(CMAKE_MODULE_PATH "${TEST_TEMPLATE_MODS}")
+
+add_executable(${T_APP_NAME} main.cpp)
+target_compile_definitions(
+    ${T_APP_NAME}
+    PRIVATE
+    $<$<CONFIG:Release>:ag_NDEBUG>
+    $<$<CONFIG:Debug>:ag_DEBUG _DEBUG>
+)
+target_compile_options(
+    ${T_APP_NAME}
+    PRIVATE
+    $<$<CONFIG:Release>:-O3>
+    $<$<CONFIG:Debug>:-O0 -g -Wall -Werror -fsanitize=address>
+)
+target_link_options(
+    ${T_APP_NAME}
+    PRIVATE
+    $<$<CONFIG:Release>:>
+    $<$<CONFIG:Debug>: -fsanitize=address>
+)
+target_compile_features(${T_APP_NAME} PRIVATE cxx_std_17)
+target_include_directories(
+    ${T_APP_NAME}
+    PRIVATE
+    ${GLM_INCLUDE_DIR}
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../src/include"
+)
+target_link_libraries(
+    ${T_APP_NAME}
+    PRIVATE
+    ag
+)
