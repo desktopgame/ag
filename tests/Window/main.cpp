0ded150f999837109c88ddcda8477694212e8299
koya <gnndr36@gmail.com>
2022-11-14 20:39:56 +0900

    Update: 複数ウィンドウのテスト

--- tests/Window/main.cpp
+++ tests/Window/main.cpp
@@ -1,14 +1,16 @@
 #include <ag/Engine.hpp>
+#include <ag/Looper.hpp>
 #include <ag/Window.hpp>
 #include <ag/native/glfw.hpp>
 
 int main(int argc, char* argv[])
 {
     auto engine = ag::Engine::getInstance()->startup(argc, argv);
+    auto looper = engine->getLooper();
     auto window = ag::Window::create(1280, 720, false, "");
-    window->makeContextCurrent();
-    while (!window->shouldClose()) {
-        glfwPollEvents();
+    auto window2 = ag::Window::create(1280, 720, false, "");
+    while (looper->isRunnable()) {
+        looper->run();
     }
     return 0;
 }
