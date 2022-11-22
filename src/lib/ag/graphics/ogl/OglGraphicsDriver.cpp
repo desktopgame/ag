62ec2bd6a55bb6f41cda380ede63c2d9d019870d
koya <gnndr36@gmail.com>
2022-11-22 23:35:20 +0900

    Update: glewの初期化処理

--- src/lib/ag/graphics/ogl/OglGraphicsDriver.cpp
+++ src/lib/ag/graphics/ogl/OglGraphicsDriver.cpp
@@ -1,4 +1,6 @@
 #include <ag/graphics/ogl/OglGraphicsDriver.hpp>
+#include <ag/native/glfw.hpp>
+#include <stdexcept>
 
 namespace ag {
 OglGraphicsDriver::OglGraphicsDriver()
@@ -12,6 +14,10 @@ void OglGraphicsDriver::useWindowHint()
 
 void OglGraphicsDriver::useContextExtension()
 {
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        throw std::runtime_error("glew is unavailable.");
+    }
 }
 std::shared_ptr<IGraphicsDevice> OglGraphicsDriver::getGraphicsDevice() const
 {
