9623f15afeb683b1329cb663712c96befab98bba
koya <gnndr36@gmail.com>
2022-11-22 23:02:05 +0900

    Update: 別ファイルに分離

---
+++ src/Defines.cmake
@@ -0,0 +1,9 @@
+# macro
+set(AG_RELEASE_DEFINE AG_NDEBUG)
+set(AG_DEBUG_DEFINE AG_DEBUG _DEBUG)
+# compile options
+set(AG_RELEASE_COMPILE -O3)
+set(AG_DEBUG_COMPILE -O0 -g -Wall -Werror -fsanitize=address)
+# link options
+set(AG_RELEASE_LINK "")
+set(AG_DEBUG_LINK -fsanitize=address)
