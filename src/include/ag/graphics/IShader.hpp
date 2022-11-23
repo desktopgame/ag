739409cc6b77cdb6fabe0e9468a54dbb12f82876
koya <gnndr36@gmail.com>
2022-11-23 10:19:07 +0900

    Add: シェーダーパラメータを適用するメソッドの追加

--- src/include/ag/graphics/IShader.hpp
+++ src/include/ag/graphics/IShader.hpp
@@ -2,9 +2,11 @@
 #include <memory>
 
 namespace ag {
+class IShaderParameter;
 class IShader {
     using Instance = std::shared_ptr<IShader>;
     IShader() = default;
     virtual ~IShader() = default;
+    virtual void apply(const std::shared_ptr<IShaderParameter>& parameter) = 0;
 };
 }
