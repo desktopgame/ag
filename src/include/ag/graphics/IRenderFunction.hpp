d000fb91e1e94f3d4fa934386557028b97e28c45
koya <gnndr36@gmail.com>
2022-11-23 15:28:24 +0900

    Update: RenderingContextを使うように

--- src/include/ag/graphics/IRenderFunction.hpp
+++ src/include/ag/graphics/IRenderFunction.hpp
@@ -2,15 +2,13 @@
 #include <memory>
 
 namespace ag {
-class IShader;
-class IShaderParameter;
-class IBuffer;
+class RenderingContext;
 
 class IRenderFunction {
 public:
     using Instance = std::shared_ptr<IRenderFunction>;
     IRenderFunction() = default;
     virtual ~IRenderFunction() = default;
-    virtual void draw(const std::shared_ptr<IShader>& shader, const std::shared_ptr<IShaderParameter>& parameter, const std::shared_ptr<IBuffer>& vertex, const std::shared_ptr<IBuffer>& index) = 0;
+    virtual void draw(const std::shared_ptr<IShader>& shader, const std::shared_ptr<RenderingContext>& context) = 0;
 };
 }
