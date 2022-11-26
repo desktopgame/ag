ed14942e1944a5bf69002b3b6624e8cf50a3f5f8
koya <gnndr36@gmail.com>
2022-11-27 08:29:35 +0900

    Update: Metal使用時のみマクロ定義

--- src/include/ag/native/glm.hpp
+++ src/include/ag/native/glm.hpp
@@ -1,3 +1,12 @@
 #pragma once
+
+// このマクロ定義が必要。詳細は以下
+// https://stackoverflow.com/questions/54930382/is-the-glm-math-library-compatible-with-apples-metal-shading-language
+#if __APPLE__
+#if AG_METAL
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE 1
+#endif
+#endif
+
 #include <glm/ext.hpp>
 #include <glm/glm.hpp>
