3b85c8afa2faef1e51ba420da821abd40bef327a
koya <gnndr36@gmail.com>
2022-11-26 20:31:02 +0900

    Update: MTL関連の初期化

--- src/include/ag/graphics/mtl/MtlGraphicsDevice.hpp
+++ src/include/ag/graphics/mtl/MtlGraphicsDevice.hpp
@@ -1,14 +1,18 @@
 #pragma once
 #include <ag/graphics/IGraphicsDevice.hpp>
+#include <ag/native/metal.hpp>
 
 namespace ag {
 class MtlGraphicsDevice : public IGraphicsDevice {
 public:
     explicit MtlGraphicsDevice();
+    ~MtlGraphicsDevice();
     std::shared_ptr<ITexture> newTexture(int width, int height, const uint8_t* data) const override;
     std::shared_ptr<IBuffer> newVertexBuffer() const override;
     std::shared_ptr<IBuffer> newIndexBuffer() const override;
 
 private:
+    MTL::Device* m_device;
+    MTL::CommandQueue* m_commandQueue;
 };
 }
--- src/lib/ag/graphics/mtl/MtlGraphicsDevice.cpp
+++ src/lib/ag/graphics/mtl/MtlGraphicsDevice.cpp
@@ -2,7 +2,15 @@
 
 namespace ag {
 MtlGraphicsDevice::MtlGraphicsDevice()
+    : m_device(MTL::CreateSystemDefaultDevice())
+    , m_commandQueue()
 {
+    m_commandQueue = m_device->newCommandQueue();
+}
+MtlGraphicsDevice::~MtlGraphicsDevice()
+{
+    m_device->release();
+    m_commandQueue->release();
 }
 std::shared_ptr<ITexture> MtlGraphicsDevice::newTexture(int width, int height, const uint8_t* data) const
 {
