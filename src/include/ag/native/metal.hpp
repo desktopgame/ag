654f2a651b8cbcfa9ac5758ea7939450f0ee6a0a
koya <gnndr36@gmail.com>
2022-11-26 20:07:38 +0900

    Add: インクルードガード追加

--- src/include/ag/native/metal.hpp
+++ src/include/ag/native/metal.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #if __APPLE__
+#if AG_METAL
 #include <ag/native/osx/CAMetalLayer.hpp>
 #include <ag/native/osx/MetalOne.hpp>
 #include <ag/native/osx/NSView.hpp>
 #include <ag/native/osx/NSWindow.hpp>
+#endif
 #endif
