af5bf77e166721279702b08bbfa2673c4f9af0cf
koya <gnndr36@gmail.com>
2022-11-26 13:42:44 +0900

    Fix: シェーダー修正

--- src/include/ag/graphics/ShaderCode.hpp
+++ src/include/ag/graphics/ShaderCode.hpp
@@ -3,7 +3,7 @@
 
 namespace ag {
 namespace internal {
-    static constexpr inline const char* GL_RectVertexShader = R"(
+    static constexpr inline const char* GL_ColorVertexShader = R"(
         #version 120
         attribute vec2 aVertex;
         uniform mat4 uTransformMatrix;
@@ -12,12 +12,12 @@ namespace internal {
             gl_Position = uTransformMatrix * vec4(aVertex, 0, 1);
         }
     )";
-    static constexpr inline const char* GL_RectFragmentShader = R"(
+    static constexpr inline const char* GL_ColorFragmentShader = R"(
         #version 120
-        // out vec4 outputColor;
+        uniform vec4 uColor1;
 
         void main() {
-            gl_FragColor = vec4(1, 0, 0, 1);
+            gl_FragColor = uColor1;
         }
     )";
 }
