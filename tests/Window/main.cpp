247ca92546db8f78e17389e6813aa922a9db3c87
koya <gnndr36@gmail.com>
2022-11-26 16:17:46 +0900

    Update: どっちも描画するように

--- tests/Window/main.cpp
+++ tests/Window/main.cpp
@@ -17,6 +17,7 @@ int main(int argc, char* argv[])
             }
             renderer->resize(window->getSize());
             renderer->drawCircle(glm::vec2(), glm::vec2(100, 100), glm::vec4(1, 1, 0, 0));
+            renderer->drawRect(glm::vec2(0, 100), glm::vec2(100, 100), glm::vec4(1, 0, 0, 0));
             looper->release();
         }
     }
