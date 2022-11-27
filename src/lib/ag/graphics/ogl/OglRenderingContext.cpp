c2b8ad26a333f13a56240481c6c5ab89f79dfa1e
koya <gnndr36@gmail.com>
2022-11-27 11:25:28 +0900

    Update: 必要に応じて拡張機能を使うように

--- src/lib/ag/graphics/ogl/OglRenderingContext.cpp
+++ src/lib/ag/graphics/ogl/OglRenderingContext.cpp
@@ -20,12 +20,27 @@ void OglRenderingContext::setup(const std::shared_ptr<IShader>& shader)
     auto oglVertex = std::static_pointer_cast<OglBuffer>(m_vertex);
     auto oglIndex = std::static_pointer_cast<OglBuffer>(m_index);
     if (!m_vao) {
-        glGenVertexArraysAPPLE(1, &m_vao);
-        glBindVertexArrayAPPLE(m_vao);
+#if __APPLE__
+        if (GLEW_APPLE_vertex_array_object) {
+            glGenVertexArraysAPPLE(1, &m_vao);
+            glBindVertexArrayAPPLE(m_vao);
+        } else
+#endif
+        {
+            glGenVertexArrays(1, &m_vao);
+            glBindVertexArray(m_vao);
+        }
         oglVertex->bindAsVertex(0, 2, 0, nullptr);
         oglIndex->bindAsIndex();
     } else {
-        glBindVertexArrayAPPLE(m_vao);
+#if __APPLE__
+        if (GLEW_APPLE_vertex_array_object) {
+            glBindVertexArrayAPPLE(m_vao);
+        } else
+#endif
+        {
+            glBindVertexArray(m_vao);
+        }
         oglIndex->bindAsIndex();
     }
     oglShader->use();
@@ -39,13 +54,27 @@ void OglRenderingContext::teardown(const std::shared_ptr<IShader>& shader)
     oglShader->unuse();
     oglVertex->unbindAsVertex();
     oglIndex->unbindAsIndex();
-    glBindVertexArrayAPPLE(0);
+#if __APPLE__
+    if (GLEW_APPLE_vertex_array_object) {
+        glBindVertexArrayAPPLE(0);
+    } else
+#endif
+    {
+        glBindVertexArray(0);
+    }
 }
 // private
 void OglRenderingContext::release()
 {
     if (m_vao) {
-        glDeleteVertexArraysAPPLE(1, &m_vao);
+#if __APPLE__
+        if (GLEW_APPLE_vertex_array_object) {
+            glDeleteVertexArraysAPPLE(1, &m_vao);
+        } else
+#endif
+        {
+            glDeleteVertexArrays(1, &m_vao);
+        }
         m_vao = 0;
     }
 }
