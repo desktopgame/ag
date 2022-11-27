e5cbe6ed9e445f20b7c330dae8b5b280238b75a4
koya <gnndr36@gmail.com>
2022-11-27 18:53:34 +0900

    Update: Component -> uint8_t

--- src/include/ag/graphics/ITexture.hpp
+++ src/include/ag/graphics/ITexture.hpp
@@ -5,12 +5,11 @@
 namespace ag {
 class ITexture {
 public:
-    using Component = uint8_t;
     using Instance = std::shared_ptr<ITexture>;
     ITexture() = default;
     virtual ~ITexture() = default;
 
-    virtual void update(int width, int height, const Component* pixels) = 0;
+    virtual void update(int width, int height, const uint8_t* pixels) = 0;
     virtual size_t getWidth() const = 0;
     virtual size_t getHeight() const = 0;
 };
