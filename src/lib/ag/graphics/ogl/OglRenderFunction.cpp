7b5653902f6483c65faf1fcde56a190d79447cab
koya <gnndr36@gmail.com>
2022-11-28 20:29:30 +0900

    Update: ブレンディング有効化

--- src/lib/ag/graphics/ogl/OglRenderFunction.cpp
+++ src/lib/ag/graphics/ogl/OglRenderFunction.cpp
@@ -15,6 +15,8 @@ void OglRenderFunction::begin(const std::shared_ptr<Window>& window)
     glClear(GL_COLOR_BUFFER_BIT);
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 void OglRenderFunction::draw(const std::shared_ptr<RenderingObject>& object)
 {
