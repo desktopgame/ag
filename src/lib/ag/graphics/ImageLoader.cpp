bc2f3eba237828871c0a01becb0dc08a9f1cdb74
koya <gnndr36@gmail.com>
2022-12-02 08:05:07 +0900

    Fix: 読み込み処理微修正

--- src/lib/ag/graphics/ImageLoader.cpp
+++ src/lib/ag/graphics/ImageLoader.cpp
@@ -5,11 +5,17 @@ std::unordered_map<std::string, Image> ImageLoader::s_imageMap;
 
 bool ImageLoader::load(const std::string& file, Image& img)
 {
+    bool status = false;
+    if (!s_imageMap.count(file)) {
+        Image dst;
+        if (ImageIO::load(file, ag::ImageFormat::PNG, dst)) {
+            s_imageMap.insert_or_assign(file, dst);
+        }
+    }
     if (s_imageMap.count(file)) {
         img = s_imageMap.at(file);
-        return true;
-    } else {
-        return ImageIO::load(file, ag::ImageFormat::PNG, img);
+        status = true;
     }
+    return status;
 }
 }
