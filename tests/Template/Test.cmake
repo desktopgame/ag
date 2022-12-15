b7ed94e73549bd13e7cb9f1242adecab65198251
desktopgame <gnndr36@gmail.com>
2022-12-15 19:02:36 +0900

    Update: Windowsではsanitize無効に

--- tests/Template/Test.cmake
+++ tests/Template/Test.cmake
@@ -10,18 +10,33 @@ target_compile_definitions(
     $<$<CONFIG:Release>:AG_TEST AG_NDEBUG>
     $<$<CONFIG:Debug>:AG_TEST AG_DEBUG _DEBUG>
 )
-target_compile_options(
-    ${T_APP_NAME}
-    PRIVATE
-    $<$<CONFIG:Release>:-O3>
-    $<$<CONFIG:Debug>:-O0 -g -Wall -Werror -Wno-deprecated-declarations -fsanitize=address>
-)
-target_link_options(
-    ${T_APP_NAME}
-    PRIVATE
-    $<$<CONFIG:Release>:>
-    $<$<CONFIG:Debug>: -fsanitize=address>
-)
+if (WIN32)
+    target_compile_options(
+        ${T_APP_NAME}
+        PRIVATE
+        $<$<CONFIG:Release>:-O3>
+        $<$<CONFIG:Debug>:-O0 -g -Wall -Werror -Wno-deprecated-declarations>
+    )
+    target_link_options(
+        ${T_APP_NAME}
+        PRIVATE
+        $<$<CONFIG:Release>:>
+        $<$<CONFIG:Debug>:>
+    )
+else()
+    target_compile_options(
+        ${T_APP_NAME}
+        PRIVATE
+        $<$<CONFIG:Release>:-O3>
+        $<$<CONFIG:Debug>:-O0 -g -Wall -Werror -Wno-deprecated-declarations -fsanitize=address>
+    )
+    target_link_options(
+        ${T_APP_NAME}
+        PRIVATE
+        $<$<CONFIG:Release>:>
+        $<$<CONFIG:Debug>: -fsanitize=address>
+    )
+endif()
 target_compile_features(${T_APP_NAME} PRIVATE cxx_std_17)
 target_include_directories(
     ${T_APP_NAME}
