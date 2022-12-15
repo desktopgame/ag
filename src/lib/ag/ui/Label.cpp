334941271558b09c1ea36ade449fc93ac81b94ba
koya <gnndr36@gmail.com>
2022-12-15 22:42:55 +0900

    Update: 中央に寄せるように

--- src/lib/ag/ui/Label.cpp
+++ src/lib/ag/ui/Label.cpp
@@ -7,6 +7,14 @@ Label::Label(const std::u16string& text)
 }
 void Label::update(const std::shared_ptr<ag::Renderer>& r)
 {
-    r->drawString(getLocation(), 20, m_text, getForeground());
+    auto b = getBounds();
+    auto ms = r->measureString(20, m_text);
+    if (b.size.x > ms.x) {
+        b.position.x = b.position.x + (b.size.x - ms.x) / 2;
+    }
+    if (b.size.y > ms.y) {
+        b.position.y = b.position.y + (b.size.y - ms.y) / 2;
+    }
+    r->drawString(b.position, 20, m_text, getForeground());
 }
 }
