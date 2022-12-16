ca7e6ef3679a7b13bec35eb00d5599f4971dc8ed
koya <gnndr36@gmail.com>
2022-12-16 20:12:46 +0900

    Remove: 不要なコードを削除

--- tests/UI/main.cpp
+++ tests/UI/main.cpp
@@ -15,31 +15,12 @@ public:
     {
         r->setFontMap(loadFontMap("testdata/fonts/NotoSansJP-Regular.otf"));
         loadTexture("testdata/textures/ghicon.png");
-        m_root = std::make_shared<agui::HBox>(std::vector<int> {});
-        auto tmp = std::make_shared<agui::VBox>(std::vector<int> {});
-        auto line1 = std::make_shared<agui::HBox>(std::vector<int> { 1, 2 });
-        auto line2 = std::make_shared<agui::HBox>(std::vector<int> { 1, 2 });
-        auto line3 = std::make_shared<agui::HBox>(std::vector<int> { 1, 2 });
-        line1->addComponent(std::make_shared<agui::Button>(u"こんにちは１"));
-        line1->addComponent(std::make_shared<agui::Panel>(glm::vec4 { 0.f, 1.f, 0.f, 1.f }));
-        line2->addComponent(std::make_shared<agui::Label>(u"こんにちは２"));
-        line2->addComponent(std::make_shared<agui::Panel>(glm::vec4 { 0.f, 1.f, 0.5f, 1.f }));
-        line3->addComponent(std::make_shared<agui::Label>(u"こんにちは３"));
-        line3->addComponent(std::make_shared<agui::Panel>(glm::vec4 { 0.f, 1.f, 0.f, 1.f }));
-        tmp->addComponent(line1);
-        tmp->addComponent(line2);
-        tmp->addComponent(line3);
-        m_root->addComponent(tmp);
-        m_root->addComponent(std::make_shared<agui::Panel>(glm::vec4 { 0, 1, 0, 1 }));
     }
     void update(const std::shared_ptr<ag::Window>& w, const std::shared_ptr<ag::Renderer>& r)
     {
-        m_root->doLayoutTree(agui::Rect { { 0, 0 }, w->getSize() });
-        m_root->update(r);
     }
 
 private:
-    std::shared_ptr<agui::HBox> m_root;
 };
 
 int main(int argc, char* argv[])
