b428c15364e902910ee41a1898091dec2f902a67
koya <gnndr36@gmail.com>
2022-12-18 00:04:32 +0900

    Add: consume()追加

--- src/include/ag/ui/DeviceEvent.hpp
+++ src/include/ag/ui/DeviceEvent.hpp
@@ -12,10 +12,14 @@ class DeviceEvent : public std::enable_shared_from_this<DeviceEventType> {
 public:
     using Instance = std::shared_ptr<DeviceEvent>;
     explicit DeviceEvent(DeviceEventType type);
+
     const DeviceEventType type;
 
+    void consume();
+    bool isConsumed() const;
+
 private:
-    std::shared_ptr<Component> m_source;
     DeviceEventType m_type;
+    bool m_consumed;
 };
 }
--- src/lib/ag/ui/DeviceEvent.cpp
+++ src/lib/ag/ui/DeviceEvent.cpp
@@ -3,6 +3,10 @@
 namespace agui {
 DeviceEvent::DeviceEvent(DeviceEventType type)
     : type(type)
+    , m_consumed()
 {
 }
+
+void DeviceEvent::consume() { m_consumed = true; }
+bool DeviceEvent::isConsumed() const { return m_consumed; }
 }
