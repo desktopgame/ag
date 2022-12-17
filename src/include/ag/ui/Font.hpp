eaa0e2b933573ec7d2284272849389edd641913a
koya <gnndr36@gmail.com>
2022-12-17 09:51:54 +0900

    Remove: 不要な演算子を削除

--- src/include/ag/ui/Font.hpp
+++ src/include/ag/ui/Font.hpp
@@ -6,7 +6,6 @@ class Font {
 public:
     explicit Font(std::shared_ptr<ag::FontMap> map, int size);
     Font(const Font& font);
-    Font& operator=(const Font& other);
     std::shared_ptr<ag::FontMap> map;
     int size;
 
