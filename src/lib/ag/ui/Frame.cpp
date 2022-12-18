4f4fef657e05546b22a0129ba569e8b0c056ed8c
koya <gnndr36@gmail.com>
2022-12-18 09:50:53 +0900

    Update: EventQueueへ投げるように

--- src/lib/ag/ui/Frame.cpp
+++ src/lib/ag/ui/Frame.cpp
@@ -1,3 +1,4 @@
+#include <ag/ui/EventQueue.hpp>
 #include <ag/ui/Frame.hpp>
 #include <ag/ui/MouseEvent.hpp>
 
@@ -12,14 +13,14 @@ Frame::Frame(const ag::Window::Instance& window)
     window->setCursorPosCallback([&](double xpos, double ypos) -> void {
         int ix = static_cast<int>(xpos);
         int iy = static_cast<int>(ypos);
-        dispatchEvent(std::make_shared<MouseEvent>(shared_from_this(), glm::ivec2 { ix, iy }, -1, MouseAction::None));
+        EventQueue::getInstance()->post(std::make_shared<MouseEvent>(shared_from_this(), glm::ivec2 { ix, iy }, -1, MouseAction::None));
     });
     window->setMouseButtonCallback([&](int button, int action, int mods) -> void {
         glm::ivec2 pos = window->getMousePosition();
         if (action == ag::Window::k_eventMouseActionPress) {
-            dispatchEvent(std::make_shared<MouseEvent>(shared_from_this(), pos, button, MouseAction::Press));
+            EventQueue::getInstance()->post(std::make_shared<MouseEvent>(shared_from_this(), pos, button, MouseAction::Press));
         } else {
-            dispatchEvent(std::make_shared<MouseEvent>(shared_from_this(), pos, button, MouseAction::Release));
+            EventQueue::getInstance()->post(std::make_shared<MouseEvent>(shared_from_this(), pos, button, MouseAction::Release));
         }
     });
     setBounds(Rect { { 0, 0 }, window->getSize() });
