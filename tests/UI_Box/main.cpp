3d1e62b0bafc938594ec1817805082cb0e0e486d
koya <gnndr36@gmail.com>
2022-12-18 10:56:09 +0900

    Update: フォームUIのテスト

--- tests/UI_Box/main.cpp
+++ tests/UI_Box/main.cpp
@@ -17,17 +17,16 @@ public:
     }
     void start(const std::shared_ptr<ag::Window>& w, const std::shared_ptr<ag::Renderer>& r)
     {
-        auto rootBox = std::make_shared<agui::Panel>(std::make_shared<agui::BoxLayout>(agui::Orientation::Horizontal));
-        auto vbox1 = createVBox();
-        auto vbox2 = createVBox();
-        vbox2->setBackground({ 1, 0, 0, 1 });
-        rootBox->setOpaque(true);
-        rootBox->addComponent(vbox1, nullptr);
-        rootBox->addComponent(vbox2, nullptr);
+        auto formRoot = std::make_shared<agui::Panel>(std::make_shared<agui::BoxLayout>(agui::Orientation::Vertical));
+        formRoot->addComponent(createForm(u"年齢"), nullptr);
+        formRoot->addComponent(createForm(u"名前"), nullptr);
+        formRoot->addComponent(createForm(u"身長"), nullptr);
+        formRoot->addComponent(createForm(u"体重"), nullptr);
+        formRoot->setOpaque(true);
         // create window
         m_frame = std::make_shared<agui::Frame>(w);
         m_frame->setFont(agui::Font { loadFontMap("testdata/fonts/NotoSansJP-Regular.otf"), 16 });
-        m_frame->addComponent(rootBox, std::make_shared<agui::BorderLayoutParameter>(agui::BorderPosition::Center));
+        m_frame->addComponent(formRoot, std::make_shared<agui::BorderLayoutParameter>(agui::BorderPosition::Center));
     }
     void update(const std::shared_ptr<ag::Window>& w, const std::shared_ptr<ag::Renderer>& r)
     {
@@ -35,21 +34,11 @@ public:
         m_frame->update(r);
         agui::EventQueue::getInstance()->dispatch();
     }
-    std::shared_ptr<agui::Panel> createVBox()
-    {
-        auto vbox = std::make_shared<agui::Panel>(std::make_shared<agui::BoxLayout>(agui::Orientation::Vertical));
-        vbox->setOpaque(true);
-        vbox->addComponent(createHBox(), nullptr);
-        vbox->addComponent(createHBox(), nullptr);
-        vbox->addComponent(createHBox(), nullptr);
-        return vbox;
-    }
-    std::shared_ptr<agui::Panel> createHBox()
+    std::shared_ptr<agui::Panel> createForm(const std::u16string& text)
     {
         auto hbox = std::make_shared<agui::Panel>(std::make_shared<agui::BoxLayout>(agui::Orientation::Horizontal));
-        hbox->addComponent(std::make_shared<agui::Button>(u"ボタン"), nullptr);
+        hbox->addComponent(std::make_shared<agui::Label>(text), nullptr);
         hbox->addComponent(std::make_shared<agui::TextField>(), nullptr);
-        hbox->addComponent(std::make_shared<agui::Label>(u"テキスト２"), nullptr);
         return hbox;
     }
 
@@ -60,5 +49,5 @@ private:
 int main(int argc, char* argv[])
 {
     MyApp app(argc, argv);
-    return app.main(1280, 720, true, "UI_Box");
+    return app.main(600, 400, true, "UI_Box");
 }
