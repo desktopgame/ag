6cdf11eb624b527045e226954898dcb7b5ca3a39
koya <gnndr36@gmail.com>
2022-12-18 11:34:01 +0900

    Update: テキスト描画追加

--- src/lib/ag/ui/TextField.cpp
+++ src/lib/ag/ui/TextField.cpp
@@ -6,6 +6,7 @@ namespace agui {
 TextField::TextField()
     : Component()
     , m_focusOn(false)
+    , m_text(u"")
     , m_blinkOnWait()
     , m_blinkOffWait(k_blinkOffLength)
 {
@@ -18,14 +19,17 @@ void TextField::profile()
 }
 void TextField::update(const std::shared_ptr<ag::Renderer>& r)
 {
+    auto f = getFont();
     auto bounds = getBounds();
     RenderingUtil util { r };
     const glm::vec4 buttonBack = m_focusOn ? glm::vec4 { 0.3f, 0.3f, 0.3f, 1.f } : glm::vec4 { 0.2f, 0.2f, 0.2f, 1.f };
     const glm::vec4 buttonEdge = { 0.f, 0.f, 0.f, 1.f };
     util.fillBorderedRect(bounds, k_edgeThickness, buttonBack, buttonEdge);
+    r->setFontMap(f.map);
+    r->drawString(bounds.position + glm::ivec2 { k_edgeThickness, k_edgeThickness }, f.size, m_text, getForeground());
     if (m_blinkOnWait < k_blinkOnLength) {
         if (m_focusOn) {
-            r->fillRect(bounds.position + glm::ivec2 { 3, 0 }, glm::ivec2 { 2, bounds.size.y }, glm::vec4 { 1, 1, 1, 1 });
+            r->fillRect(bounds.position + glm::ivec2 { k_edgeThickness, k_edgeThickness }, glm::ivec2 { 2, bounds.size.y - k_edgeThickness * 2 }, glm::vec4 { 1, 1, 1, 1 });
         }
         m_blinkOnWait++;
     } else {
