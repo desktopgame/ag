3fa06ca5331ebc9b03aeb5fa8a2e725bb2b39d46
koya <gnndr36@gmail.com>
2023-01-03 09:46:32 +0900

    Add: Nodeを追加

---
+++ src/include/ag/graphics/Node.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <ag/graphics/IShader.hpp>
+#include <ag/graphics/Mesh.hpp>
+#include <ag/native/glm.hpp>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace ag {
+class Node {
+public:
+    using Instance = std::shared_ptr<Node>;
+    explicit Node(const std::weak_ptr<Node> parent, const std::string& name);
+
+    void addMesh(const std::shared_ptr<Mesh>& mesh);
+    void addNode(const std::shared_ptr<Node>& node);
+    void draw(const std::shared_ptr<IShader>& shader, const glm::mat4& transform);
+
+    std::string getName() const;
+
+    void setTransform(const glm::mat4& transform);
+    glm::mat4 getTransform() const;
+
+private:
+    std::weak_ptr<Node> m_parent;
+    std::string m_name;
+    glm::mat4 m_transform;
+    std::vector<std::shared_ptr<Mesh>> m_meshes;
+    std::vector<std::shared_ptr<Node>> m_children;
+};
+}
---
+++ src/lib/ag/graphics/Node.cpp
@@ -0,0 +1,29 @@
+#include <ag/graphics/Node.hpp>
+
+namespace ag {
+Node::Node(const std::weak_ptr<Node> parent, const std::string& name)
+    : m_parent(parent)
+    , m_name(name)
+    , m_transform(1.0f)
+    , m_meshes()
+    , m_children()
+{
+}
+
+void Node::addMesh(const std::shared_ptr<Mesh>& mesh) { m_meshes.push_back(mesh); }
+void Node::addNode(const std::shared_ptr<Node>& node) { m_children.push_back(node); }
+void Node::draw(const std::shared_ptr<IShader>& shader, const glm::mat4& transform)
+{
+    glm::mat4 newTransform = transform * m_transform;
+    for (auto msh : m_meshes) {
+        msh->draw(shader, newTransform);
+    }
+    for (auto child : m_children) {
+        child->draw(shader, newTransform);
+    }
+}
+
+std::string Node::getName() const { return m_name; }
+void Node::setTransform(const glm::mat4& transform) { m_transform = transform; }
+glm::mat4 Node::getTransform() const { return m_transform; }
+}
