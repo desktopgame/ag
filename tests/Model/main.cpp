dcdd601db30802d5ebcd59a923701e2a9c6e0e71
koya <gnndr36@gmail.com>
2023-01-03 09:47:19 +0900

    Add: モデル描画テストを追加

---
+++ tests/Model/main.cpp
@@ -0,0 +1,33 @@
+#include <ag/agOne.hpp>
+#include <ag/graphics/Model.hpp>
+#include <ag/native/glm.hpp>
+#include <cassert>
+
+int main(int argc, char* argv[])
+{
+    auto engine = ag::Engine::getInstance()->startup(argc, argv);
+    auto looper = engine->getLooper();
+    auto window = ag::Window::create(1280, 720, false, "LowLevel");
+    window->makeContextCurrent();
+    // create rectangle.
+    ag::Model::Instance model = ag::Model::loadFromFile("testdata/models/Cube.fbx");
+    ag::RenderingObject::Instance rect = ag::RenderingObject::createColorRectangle(true);
+    // start main loop.
+    while (looper->nextLoop()) {
+        while (looper->nextWindow()) {
+            auto window = looper->acquire();
+            glm::vec2 pos = glm::vec2(0, 0);
+            glm::vec2 size = glm::vec2(100, 100);
+            glm::mat4 m_projMat = glm::ortho(0.0f, 1280.f, 720.f, 0.0f /* lrbt*/, -1.0f, 1.0f);
+            glm::vec4 color = glm::vec4(1, 0, 0, 1);
+            auto param = rect->getContext()->getParameter();
+            glm::mat4 transform = glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(pos, 0)),
+                glm::vec3(size, 1));
+            param->setTransform(m_projMat * transform);
+            param->setColor1(color);
+            rect->draw();
+            looper->release();
+        }
+    }
+    return 0;
+}
