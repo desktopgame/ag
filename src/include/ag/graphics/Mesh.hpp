4886d5e7e7b9188e043381b40eefc5c5362f97ca
koya <gnndr36@gmail.com>
2023-01-03 09:46:23 +0900

    Add: Meshを追加

---
+++ src/include/ag/graphics/Mesh.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <ag/graphics/IShader.hpp>
+#include <ag/graphics/Material.hpp>
+#include <ag/graphics/RenderingContext.hpp>
+#include <string>
+
+namespace ag {
+class Mesh {
+public:
+    using Instance = std::shared_ptr<Mesh>;
+    explicit Mesh(const std::string& name);
+    void updateVertex(const glm::vec3* data, int len);
+    void updateVertex(const VertexData3D* data, int len);
+    void updateIndex(const unsigned int* data, int len);
+    void draw(const std::shared_ptr<IShader>& shader, const glm::mat4& transform);
+    std::shared_ptr<Material> getMaterial() const;
+
+private:
+    std::string m_name;
+    std::shared_ptr<Material> m_material;
+    RenderingContext::Instance m_renderingContext;
+};
+}
---
+++ src/lib/ag/graphics/Mesh.cpp
@@ -0,0 +1,24 @@
+#include <ag/Engine.hpp>
+#include <ag/graphics/IGraphicsDevice.hpp>
+#include <ag/graphics/IGraphicsDriver.hpp>
+#include <ag/graphics/Mesh.hpp>
+
+namespace ag {
+Mesh::Mesh(const std::string& name)
+    : m_name(name)
+    , m_material(std::make_shared<Material>())
+    , m_renderingContext(ag::Engine::getInstance()->getGraphicsDriver()->getGraphicsDevice()->newRenderingContext())
+{
+}
+
+void Mesh::updateVertex(const glm::vec3* data, int len) { m_renderingContext->updateVertex(data, len); }
+void Mesh::updateVertex(const VertexData3D* data, int len) { m_renderingContext->updateVertex(data, len); }
+void Mesh::updateIndex(const unsigned int* data, int len) { m_renderingContext->updateIndex(data, len); }
+void Mesh::draw(const std::shared_ptr<IShader>& shader, const glm::mat4& transform)
+{
+    auto param = m_renderingContext->getParameter();
+    param->setTransform(transform);
+    m_renderingContext->draw(shader, ag::PrimitiveType::Triangles, m_renderingContext->getVertexLength() / 3);
+}
+std::shared_ptr<Material> Mesh::getMaterial() const { return m_material; }
+}
