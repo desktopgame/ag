4cee72a262374fbcd3cd282b85c60f88173f5381
koya <gnndr36@gmail.com>
2023-01-04 19:52:07 +0900

    Update: カメラを渡す方法に対応

--- tests/Model/main.cpp
+++ tests/Model/main.cpp
@@ -15,21 +15,21 @@ int main(int argc, char* argv[])
     ag::IShader::Instance shader = ag::Engine::getInstance()->getGraphicsDriver()->getShaderCompiler()->compileFromSingleSource(ag::internal::Metal_ModelVFShader);
     ag::Model::Instance model = ag::Model::loadFromFile("testdata/models/IronMan.obj");
     // start main loop.
+    ag::Camera cam;
+    cam.resize(1280, 720);
+    cam.lookAt(glm::vec3(0, 4, -4), glm::vec3(0, 0, 0), glm::vec3(0, -1, 0));
     float angle = 0.0f;
     while (looper->nextLoop()) {
         while (looper->nextWindow()) {
             auto window = looper->acquire();
-            //*
+            // 3D rendering
             engine->getGraphicsDriver()->getRenderFunction()->begin(window, ag::RenderPass::default3D());
-            glm::mat4 proj = glm::perspective(30.f, 1280.0f / 720.0f, 1.f, 1000.f);
-            glm::mat4 view = glm::lookAt(glm::vec3(0, 4, -4), glm::vec3(0, 0, 0), glm::vec3(0, -1, 0));
             glm::mat4 transform = glm::mat4(1.0f);
             transform = glm::rotate(transform, glm::radians(angle += (100.0f * looper->deltaTime())), glm::vec3(0, 1, 0));
             transform = glm::scale(transform, glm::vec3(0.01f, 0.01f, 0.01f));
-            model->getRootNode()->draw(shader, proj * view * transform);
+            model->getRootNode()->draw(shader, cam, transform);
             engine->getGraphicsDriver()->getRenderFunction()->end();
-            //*/
-            //*
+            // 2D rendering
             engine->getGraphicsDriver()->getRenderFunction()->begin(window, ag::RenderPass::default2D());
             {
                 glm::vec2 pos = glm::vec2(0, 0);
@@ -44,7 +44,6 @@ int main(int argc, char* argv[])
                 rect->draw();
             }
             engine->getGraphicsDriver()->getRenderFunction()->end();
-            //*/
             looper->release();
         }
     }
