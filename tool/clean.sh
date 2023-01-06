8456bfeffacf260bd077130263017b0265494cb0
koya <gnndr36@gmail.com>
2023-01-06 20:51:03 +0900

    Update: 削除処理拡充

--- tool/clean.sh
+++ tool/clean.sh
@@ -3,4 +3,9 @@ find . -name 'CMakeCache.txt' -o -name 'Makefile' -o -name 'CMakeFiles' | xargs 
 find . -name '*.xcodeproj' | xargs rm -rf
 find . -name '*.make' | xargs rm -rf
 find . -name '*.build' | xargs rm -rf
+find ./build/XCBuildData -name '*.build' | xargs rm -rf
+find ./build/XCBuildData -name '*.xcbuild' | xargs rm -rf
+find ./build/XCBuildData -name '*.json' | xargs rm -rf
+find ./build/XCBuildData -name '*.txt' | xargs rm -rf
+find ./build/XCBuildData -name '*.db' | xargs rm -rf
 /usr/bin/find . -type d -empty -delete
