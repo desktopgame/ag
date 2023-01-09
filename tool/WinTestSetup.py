57cf46f4579d7bcedaa11a34bbbb90c1ce051644
desktopgame <gnndr36@gmail.com>
2023-01-09 17:13:59 +0900

    Fix: glewコピーしないように

--- tool/WinTestSetup.py
+++ tool/WinTestSetup.py
@@ -8,7 +8,6 @@ external_d: str = os.path.join(os.getcwd(), 'external')
 testdata_d: str = os.path.join(os.getcwd(), 'testdata')
 freeimage_dll: str = os.path.join(external_d, 'FreeImage', 'Dist', 'x64', 'FreeImage.dll')
 freetype_dll: str = os.path.join(external_d, 'freetype-2.12.1', 'objs', 'freetype.dll')
-glew_dll: str = os.path.join(external_d, 'glew-2.1.0', 'bin', 'Release', 'x64', 'glew32.dll')
 
 if not os.path.exists(freeimage_dll):
     print(f'{freeimage_dll} is not found.')
@@ -18,10 +17,6 @@ if not os.path.exists(freetype_dll):
     print(f'{freetype_dll} is not found.')
     exit(1)
 
-if not os.path.exists(glew_dll):
-    print(f'{glew_dll} is not found.')
-    exit(1)
-
 for test in tests:
     test_d: str = os.path.join(os.getcwd(), 'tests', test)
     if not os.path.exists(test_d):
@@ -29,7 +24,6 @@ for test in tests:
     try:
         shutil.copy(freeimage_dll, test_d)
         shutil.copy(freetype_dll, test_d)
-        shutil.copy(glew_dll, test_d)
     except:
         pass
     try:
