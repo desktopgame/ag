ba6d3ad44acf43265d59dc0e1efbc18f3289e4fb
desktopgame <gnndr36@gmail.com>
2023-01-10 21:49:32 +0900

    Add: ドライバーを実装

--- src/include/ag/graphics/dx/DxGraphicsDriver.hpp
+++ src/include/ag/graphics/dx/DxGraphicsDriver.hpp
@@ -1,12 +1,32 @@
 #pragma once
 #ifdef AG_DIRECT_X
+#include <ag/graphics/IGraphicsDriver.hpp>
+#include <ag/native/dx.hpp>
+#include <vector>
 
 namespace ag {
-class DxGraphicsDriver {
+class DxGraphicsDriver : public IGraphicsDriver {
 public:
-    explicit DxGraphicsDriver() = default;
+    explicit DxGraphicsDriver();
+    ~DxGraphicsDriver();
+    void useWindowHint() override;
+    void useContextExtension() override;
+    std::shared_ptr<IGraphicsDevice> getGraphicsDevice() const override;
+    std::shared_ptr<IShaderCompiler> getShaderCompiler() const override;
+    std::shared_ptr<IRenderFunction> getRenderFunction() const override;
+    IDXGIFactory6* getDXGIFactory() const;
+    ID3D12Device* getDevice() const;
 
 private:
+    void initFactory();
+    void initAdaptor();
+    void initFeatureLevel();
+
+    HRESULT m_result;
+    IDXGIFactory6* m_dxgiFactory;
+    std::vector<IDXGIAdapter*> m_adaptors;
+    IDXGIAdapter* m_mainAdaptor;
+    ID3D12Device* m_device;
 };
 }
 #endif
---
+++ src/lib/ag/graphics/dx/DxGraphicsDriver.cpp
@@ -0,0 +1,69 @@
+#include <ag/graphics/dx/DxGraphicsDriver.hpp>
+#include <string>
+
+namespace ag {
+DxGraphicsDriver::DxGraphicsDriver()
+    : m_result(S_OK)
+    , m_dxgiFactory(nullptr)
+    , m_adaptors()
+    , m_mainAdaptor(nullptr)
+    , m_device(nullptr)
+{
+    initFactory();
+    initAdaptor();
+    initFeatureLevel();
+}
+IDXGIFactory6* DxGraphicsDriver::getDXGIFactory() const { return m_dxgiFactory; }
+ID3D12Device* DxGraphicsDriver::getDevice() const { return m_device; }
+// private
+void DxGraphicsDriver::initFactory()
+{
+    if (FAILED(m_result)) {
+        return;
+    }
+    UINT flags = 0;
+#ifdef _DEBUG
+    flags = DXGI_CREATE_FACTORY_DEBUG;
+#endif
+    m_result = CreateDXGIFactory2(flags, IID_PPV_ARGS(&m_dxgiFactory));
+}
+void DxGraphicsDriver::initAdaptor()
+{
+    if (FAILED(m_result)) {
+        return;
+    }
+    for (int i = 0;
+         m_dxgiFactory->EnumAdapters(i, &m_mainAdaptor) != DXGI_ERROR_NOT_FOUND;
+         ++i) {
+        m_adaptors.push_back(m_mainAdaptor);
+    }
+    for (auto adpt : m_adaptors) {
+        DXGI_ADAPTER_DESC adesc = {};
+        adpt->GetDesc(&adesc);
+        std::wstring strDesc = adesc.Description;
+        if (strDesc.find(L"Iris") != std::string::npos) {
+            m_mainAdaptor = adpt;
+            break;
+        }
+    }
+}
+void DxGraphicsDriver::initFeatureLevel()
+{
+    if (FAILED(m_result)) {
+        return;
+    }
+    D3D_FEATURE_LEVEL levels[] = {
+        D3D_FEATURE_LEVEL_12_1,
+        D3D_FEATURE_LEVEL_12_0,
+        D3D_FEATURE_LEVEL_11_1,
+        D3D_FEATURE_LEVEL_11_0,
+    };
+    D3D_FEATURE_LEVEL featureLevel;
+    for (auto l : levels) {
+        if (D3D12CreateDevice(m_mainAdaptor, l, IID_PPV_ARGS(&m_device)) == S_OK) {
+            featureLevel = l;
+            break;
+        }
+    }
+}
+}
