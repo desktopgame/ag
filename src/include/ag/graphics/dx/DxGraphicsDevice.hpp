b4f025df238f8bee00ffaf69d48c96e98a9004e0
desktopgame <gnndr36@gmail.com>
2023-01-10 22:21:20 +0900

    Update: IGraphcisDevice を実装

--- src/include/ag/graphics/dx/DxGraphicsDevice.hpp
+++ src/include/ag/graphics/dx/DxGraphicsDevice.hpp
@@ -1,10 +1,16 @@
 #pragma once
 #ifdef AG_DIRECT_X
+#include <ag/graphics/IGraphicsDevice.hpp>
 
 namespace ag {
-class DxGraphicsDevice {
+class DxGraphicsDevice : public IGraphicsDevice {
 public:
-    explicit DxGraphicsDevice() = default;
+    explicit DxGraphicsDevice();
+
+    std::shared_ptr<ITexture> newTexture(int width, int height, const uint8_t* data) const override;
+    std::shared_ptr<IBuffer> newVertexBuffer() const override;
+    std::shared_ptr<IBuffer> newIndexBuffer() const override;
+    std::shared_ptr<RenderingContext> newRenderingContext() const override;
 
 private:
 };
