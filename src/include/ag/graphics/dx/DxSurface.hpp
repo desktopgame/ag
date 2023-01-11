55df857362a4b90de2b5070c5268213d3f684d2e
desktopgame <gnndr36@gmail.com>
2023-01-11 21:48:59 +0900

    Add: サーフェスを追加

---
+++ src/include/ag/graphics/dx/DxSurface.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#ifdef AG_DIRECT_X
+#include <ag/Window.hpp>
+#include <ag/graphics/dx/DxGraphicsDevice.hpp>
+#include <ag/native/dx.hpp>
+#include <memory>
+#include <vector>
+
+namespace ag {
+class DxSurface {
+public:
+    using Instance = std::shared_ptr<Window>;
+    explicit DxSurface(const Window::Instance& window);
+
+private:
+    static std::shared_ptr<DxGraphicsDevice> getDevice();
+    HRESULT m_result;
+    ID3D12CommandAllocator* m_cmdAllocator;
+    ID3D12GraphicsCommandList* m_cmdList;
+    ID3D12CommandQueue* m_cmdQueue;
+    IDXGISwapChain4* m_swapChain;
+    ID3D12DescriptorHeap* m_rtvHeap;
+    std::vector<ID3D12Resource*> m_backBuffers;
+    ID3D12Fence* m_fence;
+    UINT m_fenceVal;
+};
+}
+#endif
---
+++ src/lib/ag/graphics/dx/DxSurface.cpp
@@ -0,0 +1,32 @@
+#include <ag/Engine.hpp>
+#include <ag/graphics/IGraphicsDevice.hpp>
+#include <ag/graphics/IGraphicsDriver.hpp>
+#include <ag/graphics/dx/DxGraphicsDriver.hpp>
+#include <ag/graphics/dx/DxSurface.hpp>
+
+namespace ag {
+DxSurface::DxSurface(const Window::Instance& window)
+    : m_result(S_OK)
+    , m_cmdAllocator(nullptr)
+    , m_cmdList(nullptr)
+    , m_cmdQueue(nullptr)
+    , m_swapChain(nullptr)
+    , m_rtvHeap(nullptr)
+    , m_backBuffers()
+    , m_fence(nullptr)
+    , m_fenceVal(0)
+{
+    m_cmdAllocator = getDevice()->newCommandAllocator();
+    m_cmdList = getDevice()->newCommandList(m_cmdAllocator);
+    m_cmdQueue = getDevice()->newCommandQueue();
+    m_swapChain = getDevice()->newSwapChain(m_cmdQueue, window);
+    m_rtvHeap = getDevice()->newRenderTargetViewHeap();
+    m_backBuffers = getDevice()->newRenderTargetView(m_swapChain, m_rtvHeap);
+    m_fence = getDevice()->newFence(0);
+}
+
+std::shared_ptr<DxGraphicsDevice> DxSurface::getDevice()
+{
+    return std::static_pointer_cast<DxGraphicsDevice>(Engine::getInstance()->getGraphicsDriver()->getGraphicsDevice());
+}
+}
