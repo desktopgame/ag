8401ab41af9608c451e6ac10090fabb01d9659f6
desktopgame <gnndr36@gmail.com>
2023-01-15 19:46:21 +0900

    Update: DxShaderCompilerの空実装

--- src/include/ag/graphics/dx/DxShaderCompiler.hpp
+++ src/include/ag/graphics/dx/DxShaderCompiler.hpp
@@ -1,10 +1,20 @@
 #pragma once
 #ifdef AG_DIRECT_X
+#include <ag/graphics/IShaderCompiler.hpp>
 
 namespace ag {
-class DxShaderCompiler {
+class DxShaderCompiler : public IShaderCompiler {
 public:
     explicit DxShaderCompiler() = default;
+
+    std::shared_ptr<IShader> compileFromPartedSource(const std::string& vSource, const std::string& fSource) override;
+    bool isCompilableFromPartedSource() const override;
+
+    std::shared_ptr<IShader> compileFromSingleSource(const std::string& source) override;
+    bool isCompilableFromSingleSource() const override;
+
+    std::shared_ptr<IShader> compileFromSingleFile(const std::string& file) override;
+    bool isCompilableFromSingleFile() const override;
 
 private:
 };
---
+++ src/lib/ag/graphics/dx/DxShaderCompiler.cpp
@@ -0,0 +1,31 @@
+#include <ag/graphics/dx/DxShaderCompiler.hpp>
+
+namespace ag {
+
+std::shared_ptr<IShader> DxShaderCompiler::compileFromPartedSource(const std::string& vSource, const std::string& fSource)
+{
+    return nullptr;
+}
+bool DxShaderCompiler::isCompilableFromPartedSource() const
+{
+    return false;
+}
+
+std::shared_ptr<IShader> DxShaderCompiler::compileFromSingleSource(const std::string& source)
+{
+    return nullptr;
+}
+bool DxShaderCompiler::isCompilableFromSingleSource() const
+{
+    return false;
+}
+
+std::shared_ptr<IShader> DxShaderCompiler::compileFromSingleFile(const std::string& file)
+{
+    return nullptr;
+}
+bool DxShaderCompiler::isCompilableFromSingleFile() const
+{
+    return false;
+}
+}
