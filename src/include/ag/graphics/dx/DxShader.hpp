d86e33009685ffd371bbbc00fab580f8e1abf732
desktopgame <gnndr36@gmail.com>
2023-01-15 19:42:22 +0900

    Update: DxShaderの空実装

--- src/include/ag/graphics/dx/DxShader.hpp
+++ src/include/ag/graphics/dx/DxShader.hpp
@@ -1,10 +1,12 @@
 #pragma once
 #ifdef AG_DIRECT_X
+#include <ag/graphics/IShader.hpp>
 
 namespace ag {
-class DxShader {
+class DxShader : public IShader {
 public:
     explicit DxShader() = default;
+    void apply(const std::shared_ptr<ShaderParameter>& parameter) override;
 
 private:
 };
---
+++ src/lib/ag/graphics/dx/DxShader.cpp
@@ -0,0 +1,7 @@
+#include <ag/graphics/dx/DxShader.hpp>
+
+namespace ag {
+void DxShader::apply(const std::shared_ptr<ShaderParameter>& parameter)
+{
+}
+}
