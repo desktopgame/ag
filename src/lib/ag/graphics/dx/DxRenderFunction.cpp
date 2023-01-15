bdfdc4b6461b2373d3a9cbf75ef38c2267f54ad9
desktopgame <gnndr36@gmail.com>
2023-01-15 19:39:27 +0900

    Update: 描画フロー実装

--- src/lib/ag/graphics/dx/DxRenderFunction.cpp
+++ src/lib/ag/graphics/dx/DxRenderFunction.cpp
@@ -1,16 +1,37 @@
 #ifdef AG_DIRECT_X
 #include <ag/Window.hpp>
 #include <ag/graphics/dx/DxRenderFunction.hpp>
+#include <ag/graphics/dx/DxSurface.hpp>
 
 namespace ag {
 DxRenderFunction::DxRenderFunction()
 {
 }
 
-void DxRenderFunction::link(const std::shared_ptr<Window>& window) { }
+void DxRenderFunction::link(const std::shared_ptr<Window>& window)
+{
+    if (window->getSurfaceObject().has_value()) {
+        return;
+    }
+    auto surface = std::make_shared<DxSurface>(window);
+    window->setSurfaceObject(surface);
+}
 void DxRenderFunction::begin(const std::shared_ptr<Window>& window, const RenderPass& pass) { }
 void DxRenderFunction::end() { }
-void DxRenderFunction::clear(const std::shared_ptr<Window>& window) { }
-void DxRenderFunction::present(const std::shared_ptr<Window>& window) { }
+void DxRenderFunction::clear(const std::shared_ptr<Window>& window)
+{
+    auto surface = std::make_shared<DxSurface>(window);
+    surface->transitionPresentToRender();
+    surface->clear(window->getClearColor());
+}
+void DxRenderFunction::present(const std::shared_ptr<Window>& window)
+{
+    auto surface = std::make_shared<DxSurface>(window);
+    surface->transitionRenderToPresent();
+    surface->execute();
+    surface->waitSync();
+    surface->reset();
+    surface->present();
+}
 }
 #endif
