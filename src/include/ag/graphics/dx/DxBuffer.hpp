8ff84ec73db3d7fefc8727d3794c138722859d25
desktopgame <gnndr36@gmail.com>
2023-01-15 20:05:59 +0900

    Add: DxBufferの空実装を追加

--- src/include/ag/graphics/dx/DxBuffer.hpp
+++ src/include/ag/graphics/dx/DxBuffer.hpp
@@ -1,10 +1,15 @@
 #pragma once
 #ifdef AG_DIRECT_X
+#include <ag/graphics/IBuffer.hpp>
 
 namespace ag {
-class DxBuffer {
+class DxBuffer : public IBuffer {
 public:
     explicit DxBuffer() = default;
+
+    void allocate(size_t size) override;
+    void update(const void* data) override;
+    void release() override;
 
 private:
 };
---
+++ src/lib/ag/graphics/dx/DxBuffer.cpp
@@ -0,0 +1,17 @@
+#ifdef AG_DIRECT_X
+#include <ag/graphics/dx/DxBuffer.hpp>
+
+namespace ag {
+void DxBuffer::allocate(size_t size)
+{
+}
+
+void DxBuffer::update(const void* data)
+{
+}
+
+void DxBuffer::release()
+{
+}
+}
+#endif
