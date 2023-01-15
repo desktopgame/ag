3911a2ced75ced2848c8ceaa3f9f33f4a581e041
desktopgame <gnndr36@gmail.com>
2023-01-15 20:10:01 +0900

    Update: DxTextureを生成するように

--- src/lib/ag/graphics/dx/DxGraphicsDevice.cpp
+++ src/lib/ag/graphics/dx/DxGraphicsDevice.cpp
@@ -3,6 +3,7 @@
 #include <ag/graphics/dx/DxBuffer.hpp>
 #include <ag/graphics/dx/DxGraphicsDevice.hpp>
 #include <ag/graphics/dx/DxGraphicsDriver.hpp>
+#include <ag/graphics/dx/DxTexture.hpp>
 #include <ag/native/glfw.hpp>
 #include <stdexcept>
 
@@ -11,7 +12,7 @@ DxGraphicsDevice::DxGraphicsDevice(ID3D12Device* device)
     : m_device(device)
 {
 }
-std::shared_ptr<ITexture> DxGraphicsDevice::newTexture(int width, int height, const uint8_t* data) const { return nullptr; }
+std::shared_ptr<ITexture> DxGraphicsDevice::newTexture(int width, int height, const uint8_t* data) const { return std::make_shared<DxTexture>(); }
 std::shared_ptr<IBuffer> DxGraphicsDevice::newVertexBuffer() const { return std::make_shared<DxBuffer>(); }
 std::shared_ptr<IBuffer> DxGraphicsDevice::newIndexBuffer() const { return std::make_shared<DxBuffer>(); }
 std::shared_ptr<RenderingContext> DxGraphicsDevice::newRenderingContext() const { return nullptr; }
