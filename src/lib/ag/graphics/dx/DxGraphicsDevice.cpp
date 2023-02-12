db1dee43907dd9b2a0a6e9e5c71b9cff5d024193
desktopgame <gnndr36@gmail.com>
2023-02-12 11:01:28 +0900

    Update: newPso()を追加

--- src/lib/ag/graphics/dx/DxGraphicsDevice.cpp
+++ src/lib/ag/graphics/dx/DxGraphicsDevice.cpp
@@ -18,5 +18,15 @@ std::shared_ptr<IBuffer> DxGraphicsDevice::newVertexBuffer() const { return std:
 std::shared_ptr<IBuffer> DxGraphicsDevice::newIndexBuffer() const { return std::make_shared<DxBuffer>(m_device); }
 std::shared_ptr<RenderingContext> DxGraphicsDevice::newRenderingContext() const { return std::make_shared<DxRenderingContext>(); }
 std::shared_ptr<DxSurface> DxGraphicsDevice::newSurface(const Window::Instance& window) const { return std::make_shared<DxSurface>(window, m_device); }
+std::shared_ptr<DxPso> DxGraphicsDevice::newPso(const std::shared_ptr<DxShader>& shader,
+    const std::shared_ptr<ShaderParameter>& shaderParameter,
+    PrimitiveType primitiveType,
+    int vertexComponent,
+    bool isUsingTexCoord)
+{
+    auto pso = std::make_shared<DxPso>(shader, shaderParameter, primitiveType, vertexComponent, isUsingTexCoord);
+    pso->init(m_device);
+    return pso;
+}
 }
 #endif
