a5d273c941164e4fa191a761c7865a25537a11e7
desktopgame <gnndr36@gmail.com>
2023-02-12 10:53:31 +0900

    Update: シェーダー更新

--- src/include/ag/graphics/ShaderCode.hpp
+++ src/include/ag/graphics/ShaderCode.hpp
@@ -290,9 +290,9 @@ namespace internal {
         cbuffer cbuff0 : register(b0) { matrix mat; }
         cbuffer cbuff1 : register(b1) { float4 color; }
 
-        Output BasicVS(float4 pos : POSITION) {
+        Output BasicVS(float2 pos : POSITION) {
             Output output;
-            output.svpos = mul(mat, pos);
+            output.svpos = mul(mat, float4(pos, 0, 1));
             output.color = color;
             return output;
         }
