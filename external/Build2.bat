11bb6295a11479e9ef1ea000c8e21eb2d5e75100
desktopgame <gnndr36@gmail.com>
2023-02-26 12:45:51 +0900

    Add: dll置き場を追加

--- external/Build2.bat
+++ external/Build2.bat
@@ -1,4 +1,6 @@
 msbuild freetype-2.12.1\builds\windows\vc2010\freetype.sln /t:build /p:Configuration=Release;Platform="x64" -m
 msbuild FreeImage\FreeImage.2017.sln /t:build /p:Configuration=Release;Platform="x64" -m
 move freetype-2.12.1\objs\x64\Release\freetype.lib freetype-2.12.1\freetype.lib
+move freetype-2.12.1\objs\x64\Release\freetype.dll dll\freetype.dll
+move FreeImage\Dist\x64\FreeImage.dll dll\FreeImage.dll
 pause
