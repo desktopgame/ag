e2cf4debd1a1556017a0f1cf99df4923a886396e
desktopgame <gnndr36@gmail.com>
2023-02-26 12:57:04 +0900

    Update: セットアップスクリプト改修

--- tool/WinTestSetup.py
+++ tool/WinTestSetup.py
@@ -3,29 +3,22 @@ import shutil
 from typing import List
 
 tests: List[str] = os.listdir('tests')
-test: str
-external_d: str = os.path.join(os.getcwd(), 'external')
+dll_d: str = os.path.join(os.getcwd(), 'external', 'dll')
 testdata_d: str = os.path.join(os.getcwd(), 'testdata')
-freeimage_dll: str = os.path.join(external_d, 'FreeImage', 'Dist', 'x64', 'FreeImage.dll')
-freetype_dll: str = os.path.join(external_d, 'freetype-2.12.1', 'objs', 'freetype.dll')
 
-if not os.path.exists(freeimage_dll):
-    print(f'{freeimage_dll} is not found.')
-    exit(1)
-
-if not os.path.exists(freetype_dll):
-    print(f'{freetype_dll} is not found.')
-    exit(1)
-
+test: str
 for test in tests:
     test_d: str = os.path.join(os.getcwd(), 'tests', test)
-    if not os.path.exists(test_d):
+    if not os.path.exists(test_d) or test == '.gitignore':
         continue
-    try:
-        shutil.copy(freeimage_dll, test_d)
-        shutil.copy(freetype_dll, test_d)
-    except:
-        pass
+    dll: str
+    for dll in os.listdir(dll_d):
+        try:
+            dll_p: str = os.path.join(dll_d, dll)
+            if dll_p.endswith('.dll'):
+                shutil.copyfile(dll_p, os.path.join(test_d, dll))
+        except:
+            pass
     try:
         shutil.copytree(testdata_d, os.path.join(test_d, 'testdata'))
     except:
