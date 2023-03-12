d98009b0d830cb76c5849ae3364cb4e1eaae6d59
koya <gnndr36@gmail.com>
2023-03-12 14:01:10 +0900

    Add: address sanitizer の誤動作に対処

--- src/Defines.cmake
+++ src/Defines.cmake
@@ -1,6 +1,6 @@
 # macro
 set(AG_RELEASE_DEFINE AG_NDEBUG)
-set(AG_DEBUG_DEFINE AG_DEBUG _DEBUG)
+set(AG_DEBUG_DEFINE AG_DEBUG _DEBUG _LIBCPP_HAS_NO_ASAN) # https://stackoverflow.com/questions/38723374/why-does-xcode-define-libcpp-has-no-asan-when-creating-an-address-sanitized-bui
 # compile options
 set(AG_RELEASE_COMPILE -O3)
 if (WIN32)
