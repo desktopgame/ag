c5148ef02ad0fd2ddf6c173fd0d5755877e7d3ec
koya <gnndr36@gmail.com>
2023-03-21 09:32:26 +0900

    Update: UI仮組み

--- tests/SpaceShooter/main.cpp
+++ tests/SpaceShooter/main.cpp
@@ -58,7 +58,11 @@ public:
         renderer->end();
         // 2D rendering
         renderer->begin(window, ag::RenderPass::default2D());
-        renderer->fillRect(glm::vec2(), glm::vec2(100, 100), glm::vec4(1, 0, 0, 1));
+        renderer->drawCircle((glm::vec2(1280, 720) - glm::vec2(120, 120)) / 2.0f, glm::vec2(120, 120), glm::vec4(0, 1, 0, 1));
+        renderer->drawCircle(glm::vec2(1280 - 200, 0), glm::vec2(200, 200), glm::vec4(1, 1, 0, 1));
+        renderer->fillRect(glm::vec2(1280 - 400, 720 - 120), glm::vec2(400, 120), glm::vec4(1, 0.5f, 0, 1));
+        renderer->fillRect(glm::vec2(1280 - 300, 720 - 120 - 120), glm::vec2(300, 120), glm::vec4(1, 0, 0, 1));
+        // renderer->fillRect(glm::vec2(), glm::vec2(100, 100), glm::vec4(1, 0, 0, 1));
         renderer->end();
     }
 
